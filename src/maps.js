281a86e96417c12da9517ce5deca69c9ff0c43c0
unknown <ybandar1902@gmail.com>
2018-10-05 23:43:20 +0100

    Map Positioned

--- src/maps.js
+++ src/maps.js
@@ -1,13 +1,13 @@
 var width = 1700;
 var height = 3000;
 
-var yOffset = 400;
+var yOffset = 0;
 
-var projection = d3.geoAlbers()
+var projection = d3.geo.albersUsa()
     .scale(1000)
     .translate([width / 2, height / 2]);
 
-var path = d3.geoPath()
+var path = d3.geo.path()
     .projection(projection);
 
 
@@ -16,7 +16,10 @@ var svg = d3.select("body").append("svg")
     .attr("height", height)
 
 
-var colour = d3.interpolateLab("#000000", "#FFFFFF");
+var colour = [d3.interpolateLab("#000000", "#FFFFFF"), d3.interpolateLab("#0CE64E", "#0A3E1A")];
+
+var screenWidth = (document.body.clientWidth/2)-800;
+
 
 var i = 0;
 d3.json('us.json', function (error, us) {
@@ -26,7 +29,7 @@ d3.json('us.json', function (error, us) {
     for (var i = 0; i < 2; i++) {
         var map = svg.append('g')
             .style("stroke-width", "1.5px")
-            .attr("transform", "translate(200," + (-1200 + yOffset) + ")");
+            .attr("transform", "translate("+ screenWidth/2 + "," + (-1200 + yOffset) + ")");
 
         map.selectAll("path")
             .data(topojson.feature(us, us.objects.states).features)
@@ -34,7 +37,7 @@ d3.json('us.json', function (error, us) {
             .attr("d", path)
             .attr("class", "feature")
             .style("fill", function (d) {
-                var c = colour;
+                var c = colour[i];
                 return c(normalize(d.id));
             })
 
@@ -46,7 +49,7 @@ d3.json('us.json', function (error, us) {
             .attr("class", "mesh")
             .attr("d", path);
 
-            yOffset += 1000;
+        yOffset += 600;
     }
 
 
