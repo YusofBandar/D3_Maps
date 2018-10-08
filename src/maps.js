a7312c1d7faad176eaff066f9942f9f4f13bbc5a
unknown <ybandar1902@gmail.com>
2018-10-08 16:37:09 +0100

    Map Label Ids

--- src/maps.js
+++ src/maps.js
@@ -18,7 +18,7 @@ var svg = d3.select("body").append("svg")
 
 var colour = [d3.interpolateLab("#000000", "#FFFFFF"), d3.interpolateLab("#0CE64E", "#0A3E1A")];
 
-var screenWidth = (document.body.clientWidth/2)-800;
+var screenWidth = (document.body.clientWidth / 2) - 800;
 
 
 var i = 0;
@@ -29,7 +29,8 @@ d3.json('us.json', function (error, us) {
     for (var i = 0; i < 2; i++) {
         var map = svg.append('g')
             .style("stroke-width", "1.5px")
-            .attr("transform", "translate("+ screenWidth/2 + "," + (-1200 + yOffset) + ")");
+            .attr("transform", "translate(" + screenWidth / 2 + "," + (-1200 + yOffset) + ")")
+
 
         map.selectAll("path")
             .data(topojson.feature(us, us.objects.states).features)
@@ -42,6 +43,9 @@ d3.json('us.json', function (error, us) {
             })
 
 
+
+
+
         map.append("path")
             .datum(topojson.mesh(us, us.objects.states, function (a, b) {
                 return a !== b;
@@ -49,13 +53,33 @@ d3.json('us.json', function (error, us) {
             .attr("class", "mesh")
             .attr("d", path);
 
+
+
+        map.selectAll(".place-label")
+            .data(topojson.feature(us, us.objects.states).features)
+            .enter().append("text")
+            .attr("class", "place-label")
+            .attr("transform", function (d) {
+                console.log(d.geometry.coordinates[0][0][0]);
+                return "translate(" + projection(d.geometry.coordinates[0][0][3]) + ")";
+            })
+            .attr("dy", ".35em")
+            .text(function (d) {
+                return d.id;
+            }).style("fill","#FF0000")
+            .moveToFront();
+
         yOffset += 600;
     }
 
 
 })
 
-
+d3.selection.prototype.moveToFront = function () {
+    return this.each(function () {
+        this.parentNode.appendChild(this);
+    });
+};
 
 function normalize(val) {
     var result = (val - 0) * (1 - 0.1) / (54 - 1) + 0.1
